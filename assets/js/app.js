15e40488d2fdbb679f01cc7fb04bc4f826cbf6dc
Constanza Campos <constanza.campos@virginiogomez.cl>
2026-01-16 22:46:24 -0300

    Proyecto integrador Sprint 2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,75 +1,188 @@
-// // * 1. Crear funcion para la busqueda
-// //   TODO : Revisar api para conocer los parametros que se le pueden pasar
-// // * 2. Vincular funcion al boton del formulario, al escuchar el evento
-// // * 3. Funcion retorna una lista de objetos, se recorre la lista para crear cards con la info
-// //  [{}, {}, {}]
+// Traducir texto español a ingles para el ingrediente
+const ingredienteEspaniolaIngles = async (texto) => {
+  try {
+    // langpair se puede modificar para otra traduccion
+    const res = await fetch(
+      `https://api.mymemory.translated.net/get?q=${texto}&langpair=es|en`
+    );
 
-// async function buscarRecetas(ingrediente) {
-//   const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingrediente}`;
+    // const datos = await res.json();
+    const {
+      responseData: { translatedText },
+    } = await res.json();
+    console.log("Traduccion", translatedText);
+    return translatedText;
+  } catch (error) {
+    console.error("Error al traducir:", error);
+    return texto;
+  }
+};
 
-//   try {
-//     const respuesta = await fetch(url);
-//     const datos = await respuesta.json();
+// Traducir texto ingles a español para las instrucciones
+const instruccionInglesaEspañol = async (texto) => {
+  if (!texto) return "";
 
-//     console.log("Recetas encontradas:", datos.meals);
-//     return datos.meals;
-//   } catch (error) {
-//     console.error("Error al buscar la receta:", error);
-//   }
-// }
+  // Se divide el texto en oraciones (usando el punto como separador)
+  const oraciones = texto.split(".");
+  const chunks = [];
+  let chunkActual = "";
 
-// // buscarRecetas("beef");
+  // las Oraciones en pedazos de menos de 450 caracteres
+  oraciones.forEach((oracion) => {
+    if (chunkActual.length + oracion.length < 450) {
+      chunkActual += oracion + ".";
+    } else {
+      chunks.push(chunkActual);
+      chunkActual = oracion + ".";
+    }
+  });
+  if (chunkActual) chunks.push(chunkActual);
 
-// document.getElementById("btn-buscar").addEventListener("click", async () => {
-//   const ingrediente = document.getElementById("input-ingrediente").value.trim();
-//   const contenedor = document.getElementById("resultado");
+  try {
+    // Se traducen los fragmentos en paralelo.
+    // Chunks y promiseAll no lo hemos visto en clases aun
+    const promesas = chunks.map(async (fragmento) => {
+      const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(
+        fragmento
+      )}&langpair=en|es`;
+      const res = await fetch(url);
+      const {
+        responseData: { translatedText },
+      } = await res.json();
+      return translatedText;
+    });
 
-//   contenedor.innerHTML = "<p>Cargando...</p>";
+    const resultados = await Promise.all(promesas);
 
-//   const recetas = await buscarRecetas(ingrediente);
+    // Unimos nuevamente la data
+    return resultados.join(" ");
+  } catch (error) {
+    console.error("Error traduciendo fragmentos:", error);
+    return texto; // Fallback: devolvemos el original
+  }
+};
 
-//   if (!recetas) {
-//     contenedor.innerHTML = "<p>No se encontraron recetas.</p>";
-//     return;
-//   }
+// Busqueda de receta por ingrediente
+const buscarRecetas = async (ingrediente) => {
+  if (!ingrediente) return null;
+  const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingrediente}`;
 
-//   contenedor.innerHTML = recetas
-//     .map(
-//       (meal) => `
-//         <div class="col-12 col-md-6 col-lg-4">
-//          <div class="recipe-card card h-100 shadow-sm border-0">
-//             <img
-//               src="${meal.strMealThumb}"
-//               class="card-img-top"
-//               alt="${meal.strMeal}"
-//             />
-//             <div class="card-body">
-//               <h5 class="card-title fw-semibold">${meal.strMeal}</h5>
-//               <a href="#" class="btn btn-outline-primary rounded-pill"
-//                 >Ver receta</a
-//               >
-//             </div>
-//           </div>
-//         </div>
-//       `
-//     )
-//     .join("");
-// });
+  try {
+    const respuesta = await fetch(url);
+    const datos = await respuesta.json();
+    // console.log("Recetas encontradas:", datos.meals);
+    return datos.meals;
+  } catch (error) {
+    console.error("Error al buscar la receta:", error);
+  }
+};
 
-// Js Minimo para activar el modal con la imagen y el titulo
-document.addEventListener("click", (e) => {
+// Detalle de la receta por ID
+const detalleReceta = async (id) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+  try {
+    const respuesta = await fetch(url);
+    const datos = await respuesta.json();
+    // console.log(datos);
+    return datos.meals[0];
+  } catch (error) {
+    console.error("Error al buscar la receta:", error);
+  }
+};
+
+// muestro resultados de la busqueda
+const renderizarBusqueda = async () => {
+  const ingrediente = input.value.trim();
+  const errorMessage = document.getElementById("error-message");
+  const contenedor = document.getElementById("resultado");
+
+  // Validacion input
+  if (ingrediente === "") {
+    contenedor.innerHTML = "";
+    errorMessage.classList.remove("d-none");
+    input.focus();
+
+    return;
+  }
+
+  errorMessage.classList.add("d-none");
+
+  contenedor.innerHTML = `
+  <div class="w-100 d-flex justify-content-center">
+    <div class="spinner-border text-success" role="status">
+      <span class="visually-hidden">Buscando</span>
+    </div>
+  </div>`;
+
+  const ingredienteTraducido = await ingredienteEspaniolaIngles(ingrediente);
+  const recetas = await buscarRecetas(ingredienteTraducido);
+
+  if (!recetas) {
+    contenedor.innerHTML = `
+     <div class="col-12 d-flex justify-content-center">
+      <div class="alert alert-warning text-center shadow-sm w-100" role="alert" style="max-width: 500px;">
+        <h5 class="mb-2">😕 No se encontraron recetas.</h5>
+        <p class="mb-0">Intenta con otro ingrediente.</p>
+      </div>
+    </div>`;
+    return;
+  }
+
+  contenedor.innerHTML = recetas
+    .map(
+      ({ strMealThumb, strMeal, idMeal }) => `
+        <div class="col-12 col-md-6 col-lg-4">
+         <div class="recipe-card card h-100 shadow-sm border-0">
+            <img
+              src="${strMealThumb}"
+              class="card-img-top"
+              alt="${strMeal}"
+            />
+            <div class="card-body">
+              <h5 class="card-title fw-semibold">${strMeal}</h5>
+              <a href="#" class="btn btn-outline-success rounded-pill ver-receta" data-id=${idMeal}
+                >Ver receta</a
+              >
+            </div>
+          </div>
+        </div>
+      `
+    )
+    .join("");
+};
+
+// Ejecuto busqueda si presiono enter en el input
+const input = document.getElementById("input-ingrediente");
+input.addEventListener("keydown", async (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    renderizarBusqueda();
+  }
+});
+
+// Ejecuto busqueda mediante el boton buscar
+const searchBtn = document.getElementById("btn-buscar");
+searchBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+  renderizarBusqueda();
+});
+
+// En escucha de botones ver receta.
+document.addEventListener("click", async (e) => {
   if (!e.target.classList.contains("ver-receta")) return;
+  e.preventDefault();
 
-  const boton = e.target;
+  const id = e.target.dataset.id;
 
-  // Obtener datos del botón
-  const titulo = boton.dataset.title;
-  const imagen = boton.dataset.img;
-
+  const { strMeal, strMealThumb, strInstructions } = await detalleReceta(id);
+  const instruccionesTraducidas = await instruccionInglesaEspañol(
+    strInstructions
+  );
   // Pasarlos al modal
-  document.getElementById("modalTitulo").textContent = titulo;
-  document.getElementById("modalImagen").src = imagen;
-  document.getElementById("modalImagen").alt = titulo;
+  document.getElementById("modalTitulo").textContent = strMeal;
+  document.getElementById("modalImagen").src = strMealThumb;
+  document.getElementById("modalImagen").alt = strMeal;
+  document.getElementById("modalDetalle").textContent = instruccionesTraducidas;
 
   // Mostrar modal
   const modal = new bootstrap.Modal(document.getElementById("modalReceta"));
